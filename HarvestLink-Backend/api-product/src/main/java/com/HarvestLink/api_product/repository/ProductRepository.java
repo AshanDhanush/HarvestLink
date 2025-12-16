48804436bca00f8bf216494db6b27f48465b8486
Nuwanshi Indeewari <nuwanshiindeewari@gmail.com>
2025-12-16 16:42:35 +0530

    Add product CRUD API with MongoDB integration
    Implemented a new product service in the api-product module, including ProductController, service, repository, and DTOs for CRUD operations. Integrated MongoDB for persistence and updated application properties for database configuration. Adjusted project configuration for annotation processing and Lombok version.

---
+++ HarvestLink-Backend/api-product/src/main/java/com/HarvestLink/api_product/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.HarvestLink.api_product.repository;
+
+import com.HarvestLink.api_product.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
